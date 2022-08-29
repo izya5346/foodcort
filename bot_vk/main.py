1c97e19c80d2fdeb1aaae5b1da200bf91f8ee50b
izya5346 <karimov2203@gmail.com>
2022-08-29 15:04:19 +0300

    new funcs

--- bot_vk/main.py
+++ bot_vk/main.py
@@ -1,2 +1,14 @@
-from vkbottle import GroupEventType, GroupTypes, Keyboard, Text, VKAPIError
-from vkbottle.bot import Bot, Message
+from pprint import pprint
+from config import *
+@bot.on.message()
+async def echo(message: Message):
+    match message.text:
+        case 'Начать':
+            cities = await api.getCities()
+            
+            keyboard = Keyboard(one_time = True).schema(generate_keyboard(cities)).get_json()
+            pprint(generate_keyboard(cities))
+            await message.answer('Из какого вы города?', keyboard  = keyboard)
+        case _:
+            await message.answer(message.text)
+bot.run_forever()
